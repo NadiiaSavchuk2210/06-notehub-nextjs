1268197fed7a9ccaaabf3319471e97f6cf586cfa
NadiiaSavchuk2210 <nadiia.savchuk.g@gmail.com>
2025-10-18 17:16:52 +0300

    update Loader component

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -1,19 +1,14 @@
 import css from './Loader.module.css'
 
 interface LoaderProps {
-    isDeleting?: boolean;
     isCreating?: boolean;
 }
 
-export default function Loader({ isDeleting = false, isCreating = false }: LoaderProps) {
+export default function Loader({ isCreating = false }: LoaderProps) {
 
     return (
         <div className={css.loaderContainer}>
-            {isDeleting
-                ? <span className={css.loaderDeleting}>Deleting</span>
-                : <span className={css.loader}></span>
-            }
-            {isCreating && <span className={css.loaderCreating}></span>}
+            <span className={isCreating ? `${css.loaderProgress}` : `${css.loader}`}></span>
         </div>
     )
 }
